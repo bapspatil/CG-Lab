111e9c475d2a13d52491deb317c54aa9a2090f20
Bapusaheb Patil <bapoo1998@gmail.com>
2018-05-24 23:38:31 +0530

    Easier ScanLine.cpp

--- 9-ScanLine.cpp
+++ 9-ScanLine.cpp
@@ -1,113 +1,91 @@
-//Scan Line Algorithm
-#include <GL/glut.h>
-#include <stdio.h>
+#include<stdio.h>
+#include<GL/glut.h>
+float x1,y1,x2,y2,x3,y3,x4,y4;
+void init(){
 
-//Global Edge table
-// et contains information as ymin , xmin, ymax,1/m 
-float et[4][4]={{100,250,200,-1}, //ab
-{100,250,200,1},// ad
-{200,150,300,1},//bc
-{200,350,300,-1}};// dc
+//	glColor3f(1,0,0);
 
-
-int np=4;// number of points
-
-
-float ae[4][3];
-//js is scan line
-// find minimum value of scan
-
-float js=et[0][0];
-int iaet=0;
-int ymax=0;//max val of scan
-
-//add to a e t
-void addaet()
-{
-    int i;
-    for(i=0; i<np;i++)
-    {
-        printf("scan line =%f  & iate=%d\n",js,iaet);
-        if(js==et[i][0]) 
-        {
-            ae[iaet][0]=et[i][1];//x min
-            ae[iaet][1]=et[i][2];//ymax
-            ae[iaet][2]=et[i][3];//1/m
-            if(ae[iaet][1]>ymax) ymax=ae[iaet][1];//update ymax value
-            iaet++;
-        }
-
-    }
-
-}
-// update  aet
-void upaet()
-{
-    int i;
-    for(i=0;i<np;i++)
-        ae[i][0]=ae[i][0]+ae[i][2];//x=x+1/m
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0,500.0,0,500.0);
+	glMatrixMode(GL_MODELVIEW);
 }
 
-void draw_pixel(float x1,float x2)
-{
-    float n;
-    for(n=x1;n<x2;n++)// fill from x1 to x2
-    {
-    glColor3f(1.0,1.0,0.0);
-    glBegin(GL_POINTS);
-    glVertex2f(n,js);
-    glEnd();
-    glFlush();
-    }
-    printf(" x1=%f  x2= %f\n",x1,x2); 
+void drawpixel(float x,float y){
+	printf("\nx=%f\ty=%f",x,y);
+	glColor3f(1,0,1);
+	glPointSize(1);
+	glBegin(GL_POINTS);
+	glVertex2f(x,y);
+	glEnd();
+	glFlush();
 }
 
-void fill_tri()
-{
-    float x[3]={0,0,0};
-    int i=0;
-    bool done=false;
-do
-{
-    i=0;
-    addaet();
-        printf("  1=%f   2= %f  %f\n",ae[0][1],ae[1][1],js);
-
-    if(ae[0][1]>js){x[i]=ae[0][0]; i++; printf(" 1=%f ae=%f \n ",ae[0][0],x[0]);}
-    if(ae[1][1]>js){x[i]=ae[1][0]; i++;printf(" 1=%f ae=%f \n",ae[0][0],x[0]);}
-    if(ae[2][1]>js){x[i]=ae[2][0]; i++;}
-    if(ae[3][1]>js){x[i]=ae[3][0]; i++;}
-    draw_pixel(x[0],x[1]);
-    upaet();
-    js++;
+void edgedetect(float x1, float y1, float x2, float y2, int *le, int *re){
+	int mx,y;
+	float temp,x;
+	if((y2-y1)<0){
+		temp=y1;y1=y2;y2=temp;
+		temp=x1;x1=x2;x2=temp;
+	}
+	if((y2-y1)!=0)
+		mx=(x2-x1)/(y2-y1);
+	else
+		mx=x2-x1;
+	x=x1;
+	for(y=y1;y<=y2;y++){
+		if(x<(float)le[y])
+			le[y]=(int)x;
+		if(x>(float)re[y])
+			re[y]=(int)x;
+		x+=mx;
+	}
 }
-while(js<=ymax);
+
+void scanfill(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4){
+	int le[500],re[500];
+	int i,y;
+	for(i=0;i<500;i++){
+		le[i]=500;
+		re[i]=0;
+	}
+	edgedetect(x1,y1,x2,y2,le,re);
+	edgedetect(x2,y2,x3,y3,le,re);
+	edgedetect(x3,y3,x4,y4,le,re);
+	edgedetect(x4,y4,x1,y1,le,re);
+	for(y=0;y<500;y++){
+		if(le[y]<re[y]){
+			for(i=le[y];i<=re[y];i++){
+				drawpixel(i,y);
+			}
+		}
+	}
 }
 
-void display()
-{
-
-glClear(GL_COLOR_BUFFER_BIT);    
-glClearColor(1.0,1.0,1.0,1.0);
-fill_tri();
-glFlush();
+void display(){
+	x1=200.0,y1=200.0,x2=100.0,y2=300.0,x3=200.0,y3=400.0,x4=300.0,y4=300.0;
+	glClearColor(1,1,1,1);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0,0,1);
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(x1,y1);
+	glVertex2f(x2,y2);
+	glVertex2f(x3,y3);
+	glVertex2f(x4,y4);
+	glEnd();
+	scanfill(x1,y1,x2,y2,x3,y3,x4,y4);
+	glFlush();
 }
-void myinit()
-{
-    
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0.0,499.0,0.0,499.0);
-    glMatrixMode(GL_MODELVIEW);
 
-}
-void main(int argc, char** argv)
-{
-    glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-    glutInitWindowSize(500,500);
-    glutInitWindowPosition(400,300);
-    glutCreateWindow("polygon filling algorithm");
-    myinit();
-    glutDisplayFunc(display);
-    glutMainLoop();
+int main(){
+	//glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowPosition(10,10);
+	glutInitWindowSize(500,500);
+	glutCreateWindow("Scan Line");
+		init();
+	glutDisplayFunc(display);
+	//init();
+	glutMainLoop();
+	return 0;
 }
